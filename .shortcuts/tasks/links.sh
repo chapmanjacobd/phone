ef80a0d0199532cf7c8e951ade18fd690415636b
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-04-09 18:50:39 -0500

    links

--- .shortcuts/tasks/links.sh
+++ .shortcuts/tasks/links.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 SD=/storage/7728-84FA
 
-URLS=$(head -2 $SD/d/00_Metadata/tabs.txt)
+URLS=$(head -7 $SD/d/00_Metadata/tabs.txt)
 for url in $URLS; do
   ~/.shortcuts/tasks/open-browser.sh $url > /dev/null &
   # sleep 1
 done
 # wait
-# sed -i -e 1,7d $SD/d/00_Metadata/tabs.txt
+sed -i -e 1,7d $SD/d/00_Metadata/tabs.txt
