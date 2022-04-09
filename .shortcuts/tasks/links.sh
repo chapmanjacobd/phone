fe891f30bb8f245cb41cad61f410d6e2c8affec1
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-04-09 18:28:24 -0500

    links setup

--- .shortcuts/tasks/links.sh
+++ .shortcuts/tasks/links.sh
@@ -5,7 +5,7 @@ URLS=( $(head -3 $SD/d/00_Metadata/tabs.txt) )
 echo $URLS
 for url in $URLS; do
   echo $url
-  exec am start --user 0 -a android.intent.action.VIEW -W -d $url
+  nohup exec am start --user 0 -a android.intent.action.VIEW -W -d $url &
   sleep 1
 done
 
--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 termux-setup-storage
 pkg upgrade
-pkg install fish wget ffmpeg python
+pkg install fish wget ffmpeg python coreutils moreutils
 pip install yt-dlp google-api-python-client pypyp ipython trash-cli joblib rich pandas python-dotenv catt sqlite-utils mutagen tinytag fuckit tabulate natsort
 curl https://sdk.cloud.google.com | bash --override-components
 gcloud components install gsutil
