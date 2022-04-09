31f3c880458077914e4d222caca37abc740c6b3d
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-04-09 17:05:45 -0500

    links

--- .shortcuts/tasks/links.sh
+++ .shortcuts/tasks/links.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 SD=/storage/7728-84FA
 
-head -2 $SD/d/00_Metadata/tabs.txt | xargs -n1 -t -I{} bash "termux-open-url {} && sleep 5"
+head -2 $SD/d/00_Metadata/tabs.txt | xargs -n1 -t -I{} bash -c 'termux-open-url {} && sleep 5'
 # sed -i -e 1,2d ../d/00_Metadata/tabs.txt
