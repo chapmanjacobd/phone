f8f3ce720d883cd1afdca8a87b36afe6c422e43a
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-04-09 17:04:25 -0500

    links open-in-firefox

--- .shortcuts/tasks/links.sh
+++ .shortcuts/tasks/links.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 SD=/storage/7728-84FA
 
-head -2 $SD/d/00_Metadata/tabs.txt | xargs -n1 -t sh ./open-in-firefox.sh
+head -2 $SD/d/00_Metadata/tabs.txt | xargs -n1 -t -I{} "termux-open-url {} && sleep 5"
 # sed -i -e 1,2d ../d/00_Metadata/tabs.txt
--- .shortcuts/tasks/open-in-firefox.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-termux-open-url "$1" >/dev/null
-sleep 5
