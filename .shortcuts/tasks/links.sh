be0e000e0f315f53b5e62a613a41124a63cc34af
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-04-09 16:56:50 -0500

    links open-in-firefox

--- .shortcuts/tasks/links.sh
+++ .shortcuts/tasks/links.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-head -2 ../d/00_Metadata/tabs.txt | xargs -n1 -t sh ./open-in-firefox.sh
-sed -i -e 1,2d ../d/00_Metadata/tabs.txt
+head -2 ../d/00_Metadata/tabs.txt | xargs -n1 -t termux-open-url
+# sed -i -e 1,2d ../d/00_Metadata/tabs.txt
--- .shortcuts/tasks/open-in-firefox.sh
+++ .shortcuts/tasks/open-in-firefox.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-exec am start --user 0 -a android.intent.action.VIEW -n io.github.forkmaintainers.iceraven/.App  -d "$1" >/dev/null
+exec am start --user 0 -a android.intent.action.VIEW -n io.github.forkmaintainers.iceraven/.App -d "$1" >/dev/null
