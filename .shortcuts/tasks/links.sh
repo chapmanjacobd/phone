23dec16c13e88ab2dc4ff820fb3d27af0abc3d49
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-04-09 18:48:36 -0500

    links

--- .shortcuts/tasks/links.sh
+++ .shortcuts/tasks/links.sh
@@ -3,8 +3,7 @@ SD=/storage/7728-84FA
 
 URLS=$(head -2 $SD/d/00_Metadata/tabs.txt)
 for url in $URLS; do
-  # ~/.shortcuts/tasks/open-browser.sh $url > /dev/null &
-  termux-open-url $url > /dev/null &
+  ~/.shortcuts/tasks/open-browser.sh $url > /dev/null &
   # sleep 1
 done
 # wait
