f280e193e83a063e43fb446ed3cf1e4f0b8e3599
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-04-09 18:15:19 -0500

    links

--- .shortcuts/tasks/links.sh
+++ .shortcuts/tasks/links.sh
@@ -4,6 +4,8 @@ SD=/storage/7728-84FA
 URLS=( $(head -2 $SD/d/00_Metadata/tabs.txt) )
 for url in $URLS; do
   termux-open-url $url &
+  sleep 1
 done
 
+wait
 # sed -i -e 1,2d ../d/00_Metadata/tabs.txt
