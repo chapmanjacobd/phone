c0a46e4221181c26d0b8adbc052283a28dd47c94
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-04-09 17:54:20 -0500

    setup

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 termux-setup-storage
 pkg upgrade
 pkg install fish wget ffmpeg python
-pip install yt-dlp
+pip install yt-dlp google-api-python-client pypyp ipython trash-cli joblib rich pandas python-dotenv catt sqlite-utils mutagen tinytag fuckit tabulate natsort
