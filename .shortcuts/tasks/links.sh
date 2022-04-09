fda82510aae564b578a2cb378cc93791b9519da6
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-04-09 18:37:12 -0500

    links

--- .shortcuts/tasks/links.sh
+++ .shortcuts/tasks/links.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 SD=/storage/7728-84FA
 
+head -3 $SD/d/00_Metadata/tabs.txt
+
 URLS=( $(head -3 $SD/d/00_Metadata/tabs.txt) )
 echo $URLS
 for url in $URLS; do
   echo $url
-  ~/.shortcuts/tasks/open-browser.sh $url
+  ~/.shortcuts/tasks/open-browser.sh $url > /dev/null &
   # exec am start --user 0 -a android.intent.action.VIEW -W -d $url > /dev/null &
   # sleep 1
 done
