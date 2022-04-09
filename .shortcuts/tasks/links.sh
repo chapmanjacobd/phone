7f0ff9701f8f4ca0ae1a9eda1ae56872577c4d1f
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-04-09 18:10:33 -0500

    links setup

--- .shortcuts/tasks/links.sh
+++ .shortcuts/tasks/links.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 SD=/storage/7728-84FA
 
-head -2 $SD/d/00_Metadata/tabs.txt | xargs -n1 -t sh -c termux-open-url $@ &
+for url in
+  $(head -2 $SD/d/00_Metadata/tabs.txt)
+; do
+  termux-open-url $url &
+done
+
 # sed -i -e 1,2d ../d/00_Metadata/tabs.txt
--- setup.sh
+++ setup.sh
@@ -2,3 +2,7 @@ termux-setup-storage
 pkg upgrade
 pkg install fish wget ffmpeg python
 pip install yt-dlp google-api-python-client pypyp ipython trash-cli joblib rich pandas python-dotenv catt sqlite-utils mutagen tinytag fuckit tabulate natsort
+curl https://sdk.cloud.google.com | bash --override-components
+gcloud components install gsutil
+gcloud init
+# gcloud auth login
