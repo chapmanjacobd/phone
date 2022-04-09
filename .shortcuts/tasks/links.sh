78e48614c4327974e767d6d74bfd293809fa2240
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-04-09 16:48:43 -0500

    links open-in-firefox

--- .shortcuts/tasks/links.sh
+++ .shortcuts/tasks/links.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-head -7 ../d/00_Metadata/tabs.txt | xargs -n1 -t sh ./open-in-firefox.sh
-sed -i -e 1,7d ../d/00_Metadata/tabs.txt
+head -2 ../d/00_Metadata/tabs.txt | xargs -n1 -t sh ./open-in-firefox.sh
+sed -i -e 1,2d ../d/00_Metadata/tabs.txt
