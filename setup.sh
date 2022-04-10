8b88d987eecece2000bb7c262c0aa5adb5b872ca
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-04-09 20:31:01 -0500

    setup

--- setup.sh
+++ setup.sh
@@ -2,10 +2,9 @@ termux-setup-storage
 pkg upgrade
 pkg install fish wget ffmpeg python coreutils moreutils termux-api curl
 pip install yt-dlp google-api-python-client pypyp ipython trash-cli joblib rich pandas python-dotenv catt sqlite-utils mutagen tinytag fuckit tabulate natsort
-curl -L 'https://github.com/agnostic-apollo/tudo/releases/latest/download/tudo' -o "/data/data/com.termux/files/usr/bin/tudo"
-echo $owner
-ls -la /data/data/com.termux/files/usr/bin/
-curl https://sdk.cloud.google.com | bash --override-components
+curl https://sdk.cloud.google.com -o sdk.sh
+chmod +x ./sdk.sh
+./sdk.sh --override-components
 gcloud components install gsutil
 gcloud init
 # gcloud auth login
