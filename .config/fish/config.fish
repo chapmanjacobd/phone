e4ddf2bafb61c483f5f551123a64b7c1c9b31010
Jacob Chapman <chapmanjacobd@gmail.com>
2022-04-09 20:35:20 -0500

    wip

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/data/data/com.termux/files/usr/google-cloud-sdk/path.fish.inc' ]; . '/data/data/com.termux/files/usr/google-cloud-sdk/path.fish.inc'; end
