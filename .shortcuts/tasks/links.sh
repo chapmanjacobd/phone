90ed7512a987d0f604f6b76338db3534d2dabe07
Jacob Chapman <chapmanjacobd@gmail.com>
2022-04-10 17:08:24 -0500

    wip

--- .shortcuts/tasks/links.sh
+++ .shortcuts/tasks/links.sh
@@ -1,10 +1,11 @@
 #!/data/data/com.termux/files/usr/bin/bash
 SD=/storage/7728-84FA
+FILEN=${1:-tabs.txt}
 
-URLS=$(head -7 $SD/d/00_Metadata/tabs.txt)
+URLS=$(head -7 $SD/d/00_Metadata/$FILEN)
 for url in $URLS; do
   ~/.shortcuts/tasks/open-browser.sh $url > /dev/null &
   sleep 1
 done
 wait
-sed -i -e 1,7d $SD/d/00_Metadata/tabs.txt
+sed -i -e 1,7d $SD/d/00_Metadata/$FILEN
---
+++ .shortcuts/tasks/links_nsfw.sh
@@ -0,0 +1,3 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+~/.shortcuts/tasks/open-browser.sh tabs_nsfw.txt
