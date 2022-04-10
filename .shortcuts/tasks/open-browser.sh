faa228a2690a7563f8ead08547e1e3ec3ab6614f
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-04-09 19:54:35 -0500

    open-browser

--- .shortcuts/tasks/open-browser.sh
+++ .shortcuts/tasks/open-browser.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Works with multiple URLs unlike with termux-open{-url}
 
-exec am start --user 0 -a android.intent.action.VIEW -d "$1" >/dev/null
+am start --user 0 -a android.intent.action.VIEW -d "$1" >/dev/null
