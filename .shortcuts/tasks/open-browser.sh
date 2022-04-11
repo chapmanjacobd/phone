f39d66ecde48e7eb6f018b601323b3af8147289a
Jacob Chapman <chapmanjacobd@gmail.com>
2022-04-11 08:38:01 -0500

    wip

--- .shortcuts/tasks/open-browser.sh
+++ .shortcuts/tasks/open-browser.sh
@@ -1,4 +1,4 @@
 #!/data/data/com.termux/files/usr/bin/bash
 # Works with multiple URLs unlike with termux-open{-url}
 
-exec am start --user 0 -a android.intent.action.VIEW -d "$1" >/dev/null
+exec am start --user 0 -a android.intent.action.VIEW -d "$1" > ~/../tmp/url_log
