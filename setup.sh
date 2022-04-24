da76a8a4dcc8e90dfd732a98159a9c0bbf1043ff
Jacob Chapman <chapmanjacobd@gmail.com>
2022-04-24 14:03:45 -0500

    wip

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 termux-setup-storage
 pkg upgrade
-pkg install fish wget ffmpeg python coreutils moreutils termux-api curl openssh x11-repo mpv-x imagemagick
+pkg install fish wget ffmpeg python coreutils moreutils termux-api curl openssh x11-repo mpv-x imagemagick fd
 pip install yt-dlp google-api-python-client pypyp ipython trash-cli joblib rich pandas python-dotenv catt sqlite-utils mutagen tinytag fuckit tabulate natsort
 curl https://sdk.cloud.google.com -o sdk.sh
 chmod +x ./sdk.sh && ./sdk.sh
@@ -12,3 +12,5 @@ for f in ./.shortcuts/tasks/*.sh
 do
   termux-fix-shebang $f
 done
+
+git clone https://github.com/adi1090x/termux-style && cd termux-style && ./install
