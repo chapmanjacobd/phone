daca347f01b3be1e0b4f386a400d87adf905bd78
Jacob Chapman <chapmanjacobd@gmail.com>
2022-04-25 10:31:09 -0500

    wip

--- .config/fish/functions/fish_right_prompt.fish
+++ .config/fish/functions/fish_right_prompt.fish
@@ -13,7 +13,7 @@ function fish_right_prompt
     # We don't want the leading space.
     set -l vcs (fish_vcs_prompt '(%s)' 2>/dev/null)
 
-    set -l d (set_color brgrey)(date "+%R")(set_color normal)
+    #set -l d (set_color brgrey)(date "+%R")(set_color normal)
 
     set -l duration "$cmd_duration$CMD_DURATION"
     if test $duration -gt 100
@@ -28,5 +28,5 @@ function fish_right_prompt
     and set -l venv (string replace -r '.*/' '' -- "$VIRTUAL_ENV")
 
     set_color normal
-    string join " " -- $venv $duration $vcs $d
+    string join " " -- $venv $duration $vcs #$d
 end
