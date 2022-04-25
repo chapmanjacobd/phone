bb6059b64a62d642b9e8c14a88b876c6306a4161
Jacob Chapman <chapmanjacobd@gmail.com>
2022-04-25 10:32:29 -0500

    wip

--- .config/fish/functions/opentabs.fish
+++ .config/fish/functions/opentabs.fish
@@ -1,8 +1,8 @@
 function opentabs --argument qty file
-for tab in  (head -$qty $file)
-echo $tab
-firefox $tab
-end
+    for tab in (head -$qty $file)
+        echo $tab
+        ~/.shortcuts/tasks/open-browser.sh $tab
+    end
 
-sed -i -e 1,"$qty"d $file
+    sed -i -e 1,"$qty"d $file
 end
