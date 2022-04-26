557911839cd8f64a038bcda821f8761cd0a3801b
Jacob Chapman <chapmanjacobd@gmail.com>
2022-04-26 10:12:46 -0500

    wip

--- .config/fish/functions/vcut.fish
+++ .config/fish/functions/vcut.fish
@@ -1,4 +1,4 @@
 function vcut --argument file tin tout ct
     set fl (string split -r -m1 . $file)
-    ffmpeg -ss "$tin". -i $file -c copy -to "$tout". $fl[1].$ct.$fl[2]
+    ffmpeg -nostats -hide_banner -loglevel error -ss "$tin". -i $file -c copy -to "$tout". $fl[1].$ct.$fl[2]
 end
