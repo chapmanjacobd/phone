a51b15ae66be5ec1ca895b0b7aeb702775a5126c
Jacob Chapman <chapmanjacobd@gmail.com>
2022-05-22 21:05:33 -0500

    wip

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 termux-setup-storage
 pkg upgrade
-pkg install fish wget ffmpeg python coreutils moreutils termux-api curl openssh x11-repo mpv-x imagemagick fd socat file
+pkg install fish wget ffmpeg python coreutils moreutils termux-api curl openssh x11-repo mpv-x imagemagick fd socat file jq
 pip install yt-dlp google-api-python-client pypyp ipython trash-cli joblib rich pandas python-dotenv catt sqlite-utils mutagen tinytag fuckit tabulate natsort
 curl https://sdk.cloud.google.com -o sdk.sh
 chmod +x ./sdk.sh && ./sdk.sh
