66bc08fd2c442c35223bb89f943b86498d1cd09f
Jacob Chapman <chapmanjacobd@gmail.com>
2022-05-22 21:13:06 -0500

    wip

--- .config/fish/functions/randmusic.fish
+++ .config/fish/functions/randmusic.fish
@@ -1,3 +1,7 @@
 function randmusic
-    mpv --input-ipc-server=~/../tmp/mpv_socket --shuffle ~/d/80_Now_Listening/
+    if pgrep mpv >/dev/null
+        pkill mpv
+    else
+        mpv --input-ipc-server=~/../tmp/mpv_socket --shuffle ~/d/80_Now_Listening/
+    end
 end
