70dd9e0c1fb76dbde55fb74f0d2605543f2b4202
Jacob Chapman <chapmanjacobd@gmail.com>
2022-05-25 00:03:32 -0500

    wip

--- .shortcuts/tasks/ssh-sleepmusic.sh
+++ .shortcuts/tasks/ssh-sleepmusic.sh
@@ -1,3 +1,3 @@
 #!/data/data/com.termux/files/usr/bin/fish
 
-ssh xk@192.168.1.174 sleepmusic
+ssh xk@192.168.1.174 sleepmusic (termux-dialog radio -v "Bedroom speaker","Xylo and Orchestra" | jq '.text')
