b03ff117e6bb20efbbada17708f7d8b655cbe2c9
Jacob Chapman <chapmanjacobd@gmail.com>
2022-07-02 12:16:14 -0500

    wip

--- .shortcuts/tasks/video_nsfw.sh
+++ .shortcuts/tasks/video_nsfw.sh
@@ -1,3 +1,5 @@
 #!/data/data/com.termux/files/usr/bin/fish
 
-open (python lb/watch.py lb/tax.db --sort 'duration desc' -p --filename -L 1 -O)
+set vid (python lb/watch.py lb/tax.db --sort 'duration desc' -p --filename -L 1 -O)
+echo "$vid"
+open "$vid"
