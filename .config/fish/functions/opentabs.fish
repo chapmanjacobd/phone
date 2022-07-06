6ce86b9f86f2c55c84704fdb1e893db163b2fef9
Jacob Chapman <chapmanjacobd@gmail.com>
2022-07-06 11:29:54 -0500

    wip

--- .config/fish/functions/opentabs.fish
+++ .config/fish/functions/opentabs.fish
@@ -1,19 +1,22 @@
 function opentabs
     argparse 'p/prefix=' 'n/qty=' -- $argv
     set -l file $argv[1]
-    if set -q $_flag_prefix
+
+    if set -q _flag_prefix
         set pre $_flag_prefix
     else
         set pre 'https://www.google.com/search?q='
     end
 
-    if set -q $_flag_qty
+    if set -q _flag_qty
         set qty $_flag_qty
     else
         set qty 7
     end
 
-    for tab in (head -$qty $file)
+    set tabs (head -n $qty $file)
+
+    for tab in tabs
         echo $tab
         if test http = (string sub --length 4 "$tab")
             ~/.shortcuts/tasks/open-browser.sh $tab >/dev/null &
