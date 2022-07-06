5fd13a2093d386996372d61405267d94a87f88cd
Jacob Chapman <chapmanjacobd@gmail.com>
2022-07-06 07:12:09 -0500

    wip

--- .config/fish/functions/opentabs.fish
+++ .config/fish/functions/opentabs.fish
@@ -1,8 +1,20 @@
-function opentabs --argument qty file
-    for tab in (head -$qty "$file")
-        echo $tab
-        ~/.shortcuts/tasks/open-browser.sh "$tab"
+function opentabs
+    argparse p/prefix q/qty -- $argv
+    set -l file $argv[1]
+    if set -q $_flag_prefix
+        set pre $_flag_prefix
+    else
+        set pre 'https://www.google.com/search?q='
     end
-
-    sed -i -e 1,"$qty"d "$file"
+    for tab in (head -$qty $file)
+        echo $tab
+        if test http = (string sub --length 4 "$tab")
+            ~/.shortcuts/tasks/open-browser.sh $tab >/dev/null &
+        else
+            ~/.shortcuts/tasks/open-browser.sh "$pre$tab" >/dev/null &
+        end
+        sleep 1
+    end
+    wait
+    sed -i -e 1,"$qty"d $file
 end
