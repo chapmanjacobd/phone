c1cd98549ce4f19b7485b18a5dc594e772b9c9a3
Jacob Chapman <chapmanjacobd@gmail.com>
2022-08-01 23:33:28 -0500

    wip

--- .shortcuts/tasks/ssh-sleepmusic.sh
+++ .shortcuts/tasks/ssh-sleepmusic.sh
@@ -1,7 +1,12 @@
 #!/data/data/com.termux/files/usr/bin/fish
 
 set s (termux-dialog text -i relax -t Searchtext | jq -r '.text')
-set t (termux-dialog radio -v "Bedroom speaker","Xylo and Orchestra" | jq -r '.text')
+set t (termux-dialog radio -v "Bedroom pair","Xylo and Orchestra" | jq -r '.text')
 set timer (termux-dialog radio -v 1400,2700,5200 | jq -r '.text')
+
+set -q s; or set s relax
+set -q t; or set t "Bedroom pair"
+set -q timer; or set timer 480
+
 
 ssh xk@192.168.1.174 lt-search "$s" "$t" $timer
