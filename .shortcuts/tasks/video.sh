7af1e681de4c526bfbdf8ba9f415cb0d903acabd
Jacob Chapman <chapmanjacobd@gmail.com>
2022-08-12 16:36:26 -0500

    wip

--- .shortcuts/tasks/video.sh
+++ .shortcuts/tasks/video.sh
@@ -1,5 +1,5 @@
 #!/data/data/com.termux/files/usr/bin/fish
 
-set vid (python lb/watch.py lb/videos.db -p --filename -L 1 -O $argv)
+set vid (lb wt ~/lb/video.db -p f -L1 -u random)
 echo "$vid"
 open "$vid"
--- .shortcuts/tasks/video_nsfw.sh
+++ .shortcuts/tasks/video_nsfw.sh
@@ -1,5 +1,5 @@
 #!/data/data/com.termux/files/usr/bin/fish
 
-set vid (lb wt ~/lb/tax.db -p f -L)
+set vid (lb wt ~/lb/tax.db -p f -L1 -u random)
 echo "$vid"
 open "$vid"
