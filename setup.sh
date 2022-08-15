a5d3d42a40cc71b664f38b28fece7f15cf6dafeb
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-08-15 16:09:57 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,12 +2,7 @@ termux-setup-storage
 pkg upgrade
 pkg install fish wget ffmpeg python coreutils moreutils termux-api curl openssh x11-repo mpv-x imagemagick fd socat file jq netcat-openbsd
 pip install --upgrade requests pip
-pip install yt-dlp google-api-python-client pypyp ipython trash-cli joblib rich pandas python-dotenv catt sqlite-utils mutagen tinytag fuckit tabulate natsort joblib rich pandas ipython python-dotenv subliminal catt sqlite-utils mutagen tinytag fuckit tabulate natsort protobuf xklb
-curl https://sdk.cloud.google.com -o sdk.sh
-chmod +x ./sdk.sh && ./sdk.sh
-gcloud components install gsutil
-gcloud init
-# gcloud auth login
+pip install yt-dlp pypyp ipython trash-cli rich pandas catt sqlite-utils xklb
 
 for f in ./.shortcuts/tasks/*.sh
 do
