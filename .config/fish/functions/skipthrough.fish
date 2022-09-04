c10b7c36dbb4103aca31c37bc19fcdd0e8e92c41
Jacob Chapman <chapmanjacobd@gmail.com>
2022-09-04 16:17:46 -0500

    wip

---
+++ .config/fish/functions/skipthrough.fish
@@ -0,0 +1,8 @@
+function skipthrough
+
+  echo 'seek 20' | socat - ~/../tmp/mpv_socket
+sleep 2
+echo 'seek 20' | socat - ~/../tmp/mpv_socket
+sleep 1
+echo 'seek 50' | socat - ~/../tmp/mpv_socket
+end
