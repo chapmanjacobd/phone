1497bdfceb67334b3b4b5f6dcde54ca106ee217c
Jacob Chapman <chapmanjacobd@gmail.com>
2022-09-04 17:44:44 -0500

    wip

--- .shortcuts/tasks/video.sh
+++ .shortcuts/tasks/video.sh
@@ -1,5 +1,3 @@
 #!/data/data/com.termux/files/usr/bin/fish
 
-set vid (lb wt ~/lb/video.db -p f -L1 -u random | string escape)
-echo "$vid"
-open "$vid"
+lb wt ~/lb/video.db  -L1 --player termux-open
--- .shortcuts/tasks/video_nsfw.sh
+++ .shortcuts/tasks/video_nsfw.sh
@@ -1,5 +1,3 @@
 #!/data/data/com.termux/files/usr/bin/fish
 
-set vid (lb wt ~/lb/tax.db -p f -L1 -u random | string escape)
-echo "$vid"
-open "$vid"
+lb wt ~/lb/tax.db -r -L1 --player termux-open
