e2a35994adb1e318a7cc8019d1d445544a29dd36
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-09-07 16:08:46 -0500

    add crond

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,5 +2,6 @@ if status is-interactive
     bind \b backward-kill-bigword
 end
 
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/data/data/com.termux/files/usr/google-cloud-sdk/path.fish.inc' ]; . '/data/data/com.termux/files/usr/google-cloud-sdk/path.fish.inc'; end
+if not pgrep -f "crond" >/dev/null
+    echo "[Starting crond...]" && crond && echo "[OK]"
+end
