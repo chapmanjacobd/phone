155972617f6c4714e2c9bfb8be0965916a1b451a
Jacob <phone@unli.xyz>
2022-10-13 09:35:03 -0500

    wip

--- .shortcuts/tasks/video_nsfw.sh
+++ .shortcuts/tasks/video_nsfw.sh
@@ -1,4 +1,4 @@
 #!/data/data/com.termux/files/usr/bin/fish
 
 lb x  ~/lb/tax.db ~/d/60_Now_Watching/
-lb wt ~/lb/tax.db -u 'duration desc' -L1 --player termux-open
+lb wt ~/lb/tax.db -u 'play_count, duration desc' -L1 --player termux-open
