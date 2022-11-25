8a4c6b33b475daa33261a001bee92648d03b3e54
Jacob <phone@unli.xyz>
2022-11-24 19:29:00 -0600

    wip

--- .shortcuts/tasks/video.sh
+++ .shortcuts/tasks/video.sh
@@ -1,4 +1,5 @@
 #!/data/data/com.termux/files/usr/bin/fish
 
-lb x  ~/lb/video.db ~/d/70_Now_Watching/ &
-lb wt ~/lb/video.db -OOO -L1 --player termux-open
+# lb x  ~/lb/video.db ~/d/70_Now_Watching/ &
+# lb wt ~/lb/video.db -OOO -L1 --player termux-open
+open (randvid ~/d/70_Now_Watching/)
--- .shortcuts/tasks/video_nsfw.sh
+++ .shortcuts/tasks/video_nsfw.sh
@@ -1,4 +1,6 @@
 #!/data/data/com.termux/files/usr/bin/fish
 
-lb x  ~/lb/tax.db ~/d/60_Now_Watching/ &
-lb wt ~/lb/tax.db -u 'play_count, duration desc' -L1 --player termux-open
+# lb x  ~/lb/tax.db ~/d/60_Now_Watching/ &
+# lb wt ~/lb/tax.db -u 'play_count, duration desc' -L1 --player termux-open
+open (randvid ~/d/60_Now_Watching/)
+
