0b59493c5d7d2a25733406049d37bdbc727fa466
Jacob <phone@unli.xyz>
2022-11-24 18:44:22 -0600

    wip

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ termux-setup-storage
 pkg upgrade
 pkg install fish wget ffmpeg python coreutils moreutils termux-api curl openssh mpv imagemagick fd socat file jq netcat-openbsd cronie wol python-numpy
 pip install --upgrade requests pip wheel
-pip install yt-dlp pypyp ipython trash-cli rich pandas catt sqlite-utils xklb
+pip install yt-dlp pypyp ipython trash-cli rich catt sqlite-utils xklb
 
 for f in ./.shortcuts/tasks/*.sh
 do
