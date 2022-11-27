176a3233728ad14a150eb70e1d7ae11bb64b160f
Jacob <phone@unli.xyz>
2022-11-27 09:58:52 -0600

    wip

--- .config/fish/functions/nextSong.fish
+++ .config/fish/functions/nextSong.fish
@@ -2,6 +2,6 @@ function nextSong
     if pgrep -f 'lb lt'
         lb next --delete
     else
-        ssh 10400 lb next --delete
+        ssh 10400 -o RemoteCommand=none lb next --delete
     end
 end
