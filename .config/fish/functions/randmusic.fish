d0e5c997f0ae6ffcaf2f3b18ba29cfb1b22ecc86
Jacob <phone@unli.xyz>
2022-12-01 20:15:32 -0600

    wip

--- .config/fish/functions/randmusic.fish
+++ .config/fish/functions/randmusic.fish
@@ -1,6 +1,12 @@
 function randmusic
     if pgrep mpv >/dev/null
         pkill mpv
+    else if timeout 0.1 nc -z 192.168.1.174 2 2>/dev/null
+        if ssh xk@192.168.1.174 pgrep lt >/dev/null
+            ssh xk@192.168.1.174 lt-stop
+        else
+            ssh xk@192.168.1.174 lt-start
+        end
     else
         lt
     end
