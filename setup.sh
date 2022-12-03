0e91c40b650c2dc000f358374ea9698b2f2a486a
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-12-03 04:46:43 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -20,9 +20,10 @@ termux-open --view --content-type "application/vnd.android.package-archive" bin/
 wget -nc https://github.com/termux/termux-tasker/releases/download/v0.6.0/termux-tasker_v0.6.0+github-debug.apk
 wget -nc https://github.com/termux/termux-api/releases/download/v0.50.1/termux-api_v0.50.1+github-debug.apk
 wget -nc https://github.com/termux/termux-widget/releases/download/v0.13.0/termux-widget_v0.13.0+github-debug.apk
+wget -nc https://f-droid.org/F-Droid.apk
 
 ln -s $PREFIX/bin/termux-clipboard-set $PREFIX/bin/xclip
 
-# git clone https://github.com/adi1090x/termux-style && cd termux-style && ./install
+cat .ssh/id_ed25519.pub
 
 adb shell pm grant net.dinglisch.android.taskerm android.permission.SET_MEDIA_KEY_LISTENER
