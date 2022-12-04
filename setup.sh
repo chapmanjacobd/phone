0d73d754c884245e0e9380617388147b9b23d8be
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-12-03 21:50:23 -0600

    bindings init settings setup

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 termux-setup-storage
 
-pkg install fish wget ffmpeg python coreutils moreutils termux-api curl openssh mpv imagemagick fd socat file jq netcat-openbsd cronie wol python-numpy micro android-tools rsync fakeroot
+pkg install fish wget ffmpeg python coreutils moreutils termux-api curl openssh mpv imagemagick fd socat file jq netcat-openbsd cronie wol python-numpy android-tools rsync fakeroot
 pip install --upgrade requests pip wheel
 pip install yt-dlp pypyp ipython trash-cli rich catt sqlite-utils xklb
 
