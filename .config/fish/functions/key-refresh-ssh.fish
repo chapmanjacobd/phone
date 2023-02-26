8ce0881edd9733ac037a08712c70530372882adc
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-02-26 14:39:31 -0600

    key-refresh-ssh

---
+++ .config/fish/functions/key-refresh-ssh.fish
@@ -0,0 +1,11 @@
+# Defined via `source`
+function key-refresh-ssh --argument label
+    set oldkey (mktemp --tmpdir=/home/xk/.ssh/old/ id_ed25519_XXXXX)
+    mv ~/.ssh/id_ed25519 $oldkey
+    mv ~/.ssh/id_ed25519.pub $oldkey.pub
+
+    ssh-keygen -t ed25519 -C $label -q -N '' </dev/zero || true
+    cat ~/.ssh/id_ed25519.pub >>~/.ssh/authorized_keys
+    filterfile ~/.ssh/authorized_keys (cat $oldkey.pub)
+    cat ~/.ssh/id_ed25519.pub
+end
