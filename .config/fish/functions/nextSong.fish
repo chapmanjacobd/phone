b53ebc6879ec555b0b1b2aceeb235c36a6668253
Jacob <phone@unli.xyz>
2023-03-03 01:21:47 -0600

    wip

--- .config/fish/functions/nextSong.fish
+++ .config/fish/functions/nextSong.fish
@@ -1,6 +1,12 @@
 function nextSong
-    if pgrep -f 'lb lt'
-        lb next --delete
+    if pgrep mpv
+        set socket (dirname (mktemp -u))/mpv_socket
+        set song (echo '{ "command": ["get_property", "path"] }' | socat - $socket | jq -r .data)
+        rm "$song" &
+        echo 'playlist-next force' | socat - $socket
+    else if pgrep -f catt
+        rm (cat (dirname (mktemp -u))/catt_playing)
+        catt stop
     else
         ssh pakon -o RemoteCommand=none lb next --delete
     end
