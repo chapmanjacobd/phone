126c2d4927fa197a69c07e3ee33fa4dd18009ab1
Jacob <phone@unli.xyz>
2023-04-21 00:31:49 +0800

    wip

--- .config/fish/functions/abbrsave.fish
+++ .config/fish/functions/abbrsave.fish
@@ -1,10 +1,5 @@
 function abbrsave
-
     abbr -a -- $argv[1] $argv[2..-1]
-
-
-    filterfile ~/.config/fish/abbreviations "abbr -a -U -- $argv[1] "
-
+    filterfile ~/.config/fish/abbreviations "abbr -a -- $argv[1] "
     echo abbr -a -- $argv[1] (string escape -- $argv[2..-1]) >>~/.config/fish/abbreviations
-
 end
