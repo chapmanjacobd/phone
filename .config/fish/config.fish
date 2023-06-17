321346b9a6e9a11f11d085d492d23842d54ae33c
Jacob <phone@unli.xyz>
2023-06-17 09:28:54 -0500

    wip

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,6 +3,8 @@ if status is-interactive
     bind \e\[1\;5D backward-bigword
     bind \b backward-kill-bigword
     bind \cy redo
+
+    source $__fish_config_dir/abbreviations
 end
 
 if not pgrep -f "crond" >/dev/null
