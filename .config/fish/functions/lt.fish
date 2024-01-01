ee323d543e6d432728bf1d7446a271faa385066e
Jacob <phone@unli.xyz>
2024-01-01 13:07:18 -0600

    wip

--- .config/fish/functions/lt.fish
+++ .config/fish/functions/lt.fish
@@ -1,3 +1,3 @@
 function lt
-    lb lt ~/lb/audio.db -k delete-if-audiobook -w 'play_count=0' -r $argv
+    lb lt ~/lb/audio.db -k delete-if-audiobook -w 'play_count=0' -r --fetch-siblings if-audiobook $argv
 end
