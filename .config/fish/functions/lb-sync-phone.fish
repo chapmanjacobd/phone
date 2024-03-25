fdac249da9801b76ea5d48d49818927671b4cb70
Jacob <phone@unli.xyz>
2024-03-25 16:01:51 +0800

    wip

--- .config/fish/functions/lb-sync-phone.fish
+++ .config/fish/functions/lb-sync-phone.fish
@@ -1,5 +1,5 @@
 function lb-sync-phone
-    for pc in pakon pulse15
-        rsync ~/lb/audio.db $pc:/tmp/phone.db && ssh $pc -- tmux new-session -d -s playcounts 'lb copy-play-counts /home/xk/lb/audio.db /tmp/phone.db --source-prefix /storage/6E7B-7DCE/sync/audio/weekly/ --target-prefix /mnt/d/'
-    end
+    rsync ~/lb/audio.db pakon:/tmp/phone.db && ssh pakon -- tmux new-session -d -s playcounts 'lb copy-play-counts /home/xk/lb/audio.db /tmp/phone.db --source-prefix /storage/6E7B-7DCE/sync/audio/weekly/ --target-prefix /mnt/d/'
+
+    rsync ~/lb/audio.db pulse15:/tmp/phone.db && ssh pulse15 -- tmux new-session -d -s playcounts 'lb copy-play-counts /home/xk/lb/audio.db /tmp/phone.db --source-prefix /storage/6E7B-7DCE/ --target-prefix /mnt/d/'
 end
