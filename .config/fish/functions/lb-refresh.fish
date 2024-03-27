3955f92ca7fca9acbabec29ce5e158e8bd3997ce
Jacob <phone@unli.xyz>
2024-03-27 08:56:35 +0800

    wip

--- .config/fish/functions/lb-refresh.fish
+++ .config/fish/functions/lb-refresh.fish
@@ -1,4 +1,4 @@
-function refreshLibrary
+function lb-refresh
     lb fsadd --audio ~/lb/audio.db ~/sync/audio/ &
     lb fsadd ~/lb/video.db ~/sync/video/ &
     lb fsadd ~/lb/tax.db ~/sync/porn/video/ &
