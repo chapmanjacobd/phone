cea4843d180eec0583eb5f61c99b361491e49c80
Jacob <phone@unli.xyz>
2024-04-24 15:43:35 +0800

    wip

--- .config/fish/functions/links.fish
+++ .config/fish/functions/links.fish
@@ -1,11 +1,11 @@
 function links
-    ssh pakon sync_links
+    ssh -t pakon sync_links
 
     for url in (ssh pakon lb openlinks /home/xk/mc/links.db -w 'play_count=0' --cluster -c p1 --browser print)
         open-browser.sh "$url"
         sleep 1
     end
 
-    ssh pakon wip /home/xk/mc/
-    ssh pakon sync_links
+    ssh -t pakon wip /home/xk/mc/
+    ssh -t pakon sync_links
 end
