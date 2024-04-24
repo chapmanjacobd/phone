750157e0a0bd041dd1b36bc25b3327eae4c6395e
Jacob <phone@unli.xyz>
2024-04-24 15:30:25 +0800

    wip

--- .config/fish/functions/links.fish
+++ .config/fish/functions/links.fish
@@ -1,5 +1,6 @@
 function links
     for url in (ssh pakon lb openlinks ~/mc/links.db -w 'play_count=0' --cluster -c p1 --browser print)
         open-browser.sh "$url"
+        sleep 1
     end
 end
