092c568aae42f032f29f4d4df5b936cf1abb0776
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-06-13 17:37:50 +0800

    lb-sync-phone

--- .config/fish/functions/lb-sync-phone.fish
+++ .config/fish/functions/lb-sync-phone.fish
@@ -1,5 +1,5 @@
 function lb-sync-phone
-    rsync ~/lb/audio.db pakon:/tmp/phone.db && ssh pakon -- tmux new-session -d -s playcounts 'lb copy-play-counts /home/xk/lb/audio.db /tmp/phone.db --source-prefix /storage/6E7B-7DCE/sync/audio/weekly/ --target-prefix /mnt/d/'
+    rsync ~/lb/audio.db pakon:/tmp/phone.db && ssh pakon -- tmux new-session -d -s playcounts 'lb copy-play-counts /tmp/phone.db /home/xk/lb/audio.db --source-prefix /storage/6E7B-7DCE/sync/audio/weekly/ --target-prefix /mnt/d/'
 
-    rsync ~/lb/audio.db pulse15:/tmp/phone.db && ssh pulse15 -- tmux new-session -d -s playcounts 'lb copy-play-counts /home/xk/lb/audio.db /tmp/phone.db --source-prefix /storage/6E7B-7DCE/ --target-prefix /mnt/d/'
+    rsync ~/lb/audio.db pulse15:/tmp/phone.db && ssh pulse15 -- tmux new-session -d -s playcounts 'lb copy-play-counts /tmp/phone.db /home/xk/lb/audio.db --source-prefix /storage/6E7B-7DCE/ --target-prefix /mnt/d/'
 end
