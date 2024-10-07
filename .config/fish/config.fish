4848d7c9e2c950fbd35deb9253a950c53ab98dad
Jacob <phone@unli.xyz>
2024-10-07 20:45:24 +0800

    wip

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,11 @@ if status is-interactive
     bind \e\[1\;5C forward-bigword
     bind \e\[1\;5D backward-bigword
     bind \b backward-kill-bigword
+    bind \e\[99\;6u fish_clipboard_copy
+    bind \e, backward-kill-bigword yank yank
+    bind --preset \ct transpose-words
     bind \cy redo
+    bind \cY yank
 
     source $__fish_config_dir/abbreviations
 end
