3c36dfeb63efc51306fd80deef190ac046b11949
Jacob <phone@unli.xyz>
2024-10-14 12:48:37 +0800

    wip

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,18 +1,22 @@
 if status is-interactive
-    bind \e\[1\;5C forward-bigword
-    bind \e\[1\;5D backward-bigword
-    bind \b backward-kill-bigword
-    bind \e\[99\;6u fish_clipboard_copy
-    bind \e, backward-kill-bigword yank yank
-    bind --preset \ct transpose-words
+    bind \e\[1\;5C forward-word
+    bind \e\[1\;5D backward-word
+    bind \b backward-kill-path-component
+    bind \e\[1\;3C forward-bigword
+    bind \e\[1\;3D backward-bigword
+    bind \e\x7F backward-kill-bigword
     bind \cy redo
-    bind \cY yank
+    bind \ey yank
+
+    bind \e\[99\;6u fish_clipboard_copy
+    bind \e, kill-selection backward-kill-bigword yank yank
+    bind --preset \ct transpose-words
 
     source $__fish_config_dir/abbreviations
     fish_config theme choose nothing
 end
 
-if not pgrep -f "crond" >/dev/null
+if not pgrep -f crond >/dev/null
     echo "[Starting crond...]" && crond && echo "[OK]"
 end
 
