5c5ab9e4b0127c2009332ccd99d1fa8846b83d26
Jacob <phone@unli.xyz>
2024-10-30 23:59:12 +0800

    wip

--- .config/fish/functions/edit.fish
+++ .config/fish/functions/edit.fish
@@ -1,8 +1,4 @@
 function edit
-    set sp (path normalize ~/.termux/prefix/"$argv")
-
-    nano $PREFIX/"$argv"
-
-    mkdir -p (path dirname $sp)
-    cp $PREFIX/"$argv" "$sp"
+    nano "$argv"
+    lb relcp "$argv" ~/.termux/prefix/ --relative-to=$PREFIX
 end
