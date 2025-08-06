735ef433a1fbf6aca0ed6edd3a31ec7b30b28e84
Jacob <phone@unli.xyz>
2025-08-06 11:47:50 -0500

    wip

--- .termux/boot/services.sh
+++ .termux/boot/services.sh
@@ -1,11 +1,8 @@
 #!/data/data/com.termux/files/usr/bin/sh
 termux-wake-lock
 
+pkill crond; rm ../usr/var/run/crond.pid
 sv up crond
-
-if ! pgrep "crond" >/dev/null; then
-    echo "[Starting crond...]" && crond && echo "[OK]"
-fi
 
 . $PREFIX/etc/profile
 
