4aa731683098d1533e5428ba36a30d65a704371a
Jacob <phone@unli.xyz>
2025-11-13 08:21:33 -0600

    knowledge.fish: Added 1 line; Deleted text 'echo $l | sed 's|\.m\.|.|' >>~/j/lists/knowledge.list'

--- .config/fish/functions/knowledge.fish
+++ .config/fish/functions/knowledge.fish
@@ -1,7 +1,7 @@
 # Defined interactively
 function knowledge
     for l in $argv
-        echo $l | sed 's|\.m\.|.|' >>~/j/lists/knowledge.list
+        echo $l | sed 's|//m\.|//www.|' | sed 's|https://youtu.be/|https://www.youtube.com/watch?v=|' >>~/j/lists/knowledge.list
     end
     sorted ~/j/lists/knowledge.list
 end
