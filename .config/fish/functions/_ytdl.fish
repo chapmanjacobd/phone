cbd75d844af650f3b0b18617a3d5a945e8fc1869
Jacob <phone@unli.xyz>
2025-12-13 15:18:05 -0600

    Modified _ytdl.fish, yt.fish

--- .config/fish/functions/_ytdl.fish
+++ .config/fish/functions/_ytdl.fish
@@ -1,8 +1,11 @@
 function _ytdl
-    yt-dlp --user-agent "Mozilla/5.0 (X11; Linux x86_64; rv:98.0) Gecko/20100101 Firefox/98.0" \
-        -o "%(uploader)s/%(title).200B [%(id).60B].%(ext)s" --restrict-filenames \
+    yt-dlp \
+        -o "%(extractor_key,extractor,ie_key).220B/%(uploader,uploader_id,channel,channel_id,creator,artist,author,playlist_uploader,playlist_uploader_id,playlist_channel,playlist_channel_id,webpage_url_basen
+ame).220B/%(album,playlist_title,playlist,playlist_id).220B/%(title).170B_%(view_count)3.2D_[%(id).64B].%(ext)s" \
         --download-archive ~/.local/share/yt_archive.txt --retries 13 --extractor-retries 13 \
-        --extractor-args youtubetab:skip=authcheck --embed-metadata \
-        --reject-title "Trailer|Sneak Peek|Preview|Teaser|Promo|Live |Stream|Twitch|Crypto|Meetup|Montage|Bitcoin|Makeup|Apology|Clip |Top 10|Top Ten|Panel|Red Carpet Premiere|Now Playing|In Theaters| Scene|Announcement|First Look|Final Look|Outtakes|360|World Premiere|Event|TV Spot|Advert|campaign|Horror|Graphics Card| GPU | Terror| AWARD|Ceremony" \
+        --embed-metadata --no-progress --sleep-requests 1 --compat-options no-live-chat \
+        --reject-title "Trailer|Sneak Peek|Preview|Review|Teaser|Promo|Live |Stream|Twitch|Crypto|Meetup|Montage|Bitcoin|Makeup|Apology|Clip |Top 10|Top Ten|Panel|Red Carpet Premiere|Now Playing|In Theaters| Scene |Announcement|First Look|Final Look|Outtakes|360|World Premiere|Event|TV Spot|Advert|campaign|Horror|Graphics Card| GPU | Terror| AWARD|Ceremony" \
+        # --cookies-from-browser firefox \
+        # --user-agent "Mozilla/5.0 (X11; Linux x86_64; rv:98.0) Gecko/20100101 Firefox/98.0" \
         $argv
 end
--- .config/fish/functions/yt.fish
+++ .config/fish/functions/yt.fish
@@ -1,8 +1,6 @@
 function yt -w yt-dlp
-    ~/sync/video/
-    ytdl -i -f bestvideo[height<=576]+bestaudio/best[height<=576]/best --write-sub --write-auto-sub \
-        --sub-lang 'en,EN,en.*,en-*,EN.*,EN-*eng,ENG,english,English,ENGLISH' \
-        --embed-subs --compat-options no-keep-subs \
+    ytdl -i --format-sort res:576 --check-formats --format '(bestvideo[filesize<2G]+bestaudio/best[filesize<2G]/bestvideo*+bestaudio/best)[format_id!$=-drc][dynamic_range!^=HDR]/bestvideo[filesize<2G]+bestaudio/best[filesize<2G]/bestvideo*+bestaudio/best' \
+        --sub-lang 'en,EN,en.*,en-*,EN.*,EN-*eng,ENG,english,English,ENGLISH' --embed-subs \
         --match-filter "duration >? 59 & duration <? 14399 & live_status=?not_live" \
         $argv
 end
