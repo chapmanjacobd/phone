7d81f262507744ab09529a8fecec958f5f6538b3
Jacob <phone@unli.xyz>
2025-12-27 12:35:35 -0600

    fish_greeting.fish: 1 lines added, 2 lines deleted

--- .config/fish/functions/fish_greeting.fish
+++ .config/fish/functions/fish_greeting.fish
@@ -1,6 +1,5 @@
 # Defined in /tmp/fish.1r7oen/fish_greeting.fish @ line 2
 function fish_greeting
     # start at bottom of screen
-    printf '\033[%s;1H' (math $LINES - 1)
-    cat /proc/loadavg | cut -d' ' -f 2
+    printf "\033[$LINES;1H"
 end
