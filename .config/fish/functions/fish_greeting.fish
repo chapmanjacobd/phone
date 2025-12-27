efe2b119926a7c48b0611e84ce70d255baba484b
Jacob <phone@unli.xyz>
2025-12-27 12:34:40 -0600

    fish_greeting.fish: Added 4 lines

--- .config/fish/functions/fish_greeting.fish
+++ .config/fish/functions/fish_greeting.fish
@@ -1,3 +1,6 @@
+# Defined in /tmp/fish.1r7oen/fish_greeting.fish @ line 2
 function fish_greeting
-
+    # start at bottom of screen
+    printf '\033[%s;1H' (math $LINES - 1)
+    cat /proc/loadavg | cut -d' ' -f 2
 end
