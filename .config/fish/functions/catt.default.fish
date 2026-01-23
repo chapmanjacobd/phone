2ba640d8625ac1d5d11e3662a3a5bb6936208437
Jacob <phone@unli.xyz>
2026-01-22 23:03:04 -0600

    catt.default.fish: Modified text to 'git config --file=(path resolve ~/.config/catt/catt.cfg) options.device "$argv"'

--- .config/fish/functions/catt.default.fish
+++ .config/fish/functions/catt.default.fish
@@ -1,4 +1,4 @@
 # Defined interactively
 function catt.default
-    git config --file=/home/xk/.config/catt/catt.cfg options.device "$argv"
+    git config --file=(path resolve ~/.config/catt/catt.cfg) options.device "$argv"
 end
