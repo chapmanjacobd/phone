38a021c6ccdb74a07f9b9250cdb79e8fd5b4c354
Jacob <phone@unli.xyz>
2026-04-23 15:52:16 -0500

    startarchgui.sh: 4 lines added, 2 lines deleted

--- .shortcuts/tasks/startarchgui.sh
+++ .shortcuts/tasks/startarchgui.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-killall -9 termux-x11 pulseaudio virgl_test_server_android termux-wake-lock
+pkill pulseaudio
+pkill virgl_test_server_android
+pkill -9 -f termux.x11
 
 am start --user 0 -n com.termux.x11/com.termux.x11.MainActivity
 XDG_RUNTIME_DIR=${TMPDIR}
@@ -12,4 +14,4 @@ pacmd load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymou
 
 virgl_test_server_android &
 
-proot-distro login archlinux --user xk --shared-tmp -- bash -c "export DISPLAY=:0 PULSE_SERVER=tcp:127.0.0.1; dbus-launch --exit-with-session startxfce4"
+proot-distro login archlinux --user xk --shared-tmp -- bash -c "rm -rf /tmp/.X11-unix /tmp/.ICE-unix; export DISPLAY=:0 PULSE_SERVER=tcp:127.0.0.1; dbus-launch --exit-with-session startxfce4"
